48963a6931cf3094d7b775579e177d870b79e142
skaller <skaller@users.sourceforge.net>
2017-03-28 14:49:04 +0000

    travis rebuild

--- build/release/share/lib/rtl/flx_rtl_config.h
+++ build/release/share/lib/rtl/flx_rtl_config.h
@@ -1,4 +1,4 @@
-#line 1396 "/home/travis/build/felix-lang/felix/src/packages/rtl.fdoc"
+#line 1425 "/home/travis/build/felix-lang/felix/src/packages/rtl.fdoc"
 #ifndef __FLX_RTL_CONFIG_H__
 #define __FLX_RTL_CONFIG_H__
 
